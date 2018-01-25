e76ffb3aa84b9af59a2b3eff76bfe31acef219c0
Sai Sankara Rao Battula <saisankararao.battula@BNSF.com>
2018-01-25 00:06:52 -0600

    d2

--- test/WebContent/test/test.controller.js
+++ test/WebContent/test/test.controller.js
@@ -1,5 +1,7 @@
 sap.ui.controller("test.test", {
 //2
+	
+	//d2
 /**
 * Called when a controller is instantiated and its View controls (if available) are already created.
 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
